f90102d4afbbe68c5ed968a78174fd789c60538e
selvalt7 <52136675+selvalt7@users.noreply.github.com>
2024-12-30 13:43:15 +0100

    Unified segments editor

--- src/card/mcg-editor.ts
+++ src/card/mcg-editor.ts
@@ -2,38 +2,12 @@ import { fireEvent } from "custom-card-helpers";
 import { HomeAssistant } from "../ha/types";
 import { html, LitElement, nothing, css } from "lit";
 import { customElement, property, state } from "lit/decorators.js";
-import type { ModernCircularGaugeConfig, SegmentsConfig } from "./type";
-import { mdiSegment, mdiPlus, mdiClose, mdiInformationOutline } from "@mdi/js";
+import type { ModernCircularGaugeConfig } from "./type";
+import { mdiSegment, mdiInformationOutline } from "@mdi/js";
 import { hexToRgb } from "../utils/color";
 import { DEFAULT_MIN, DEFAULT_MAX, NUMBER_ENTITY_DOMAINS } from "../const";
 import memoizeOne from "memoize-one";
 import "../components/ha-form-mcg-list";
-
-const SEGMENT = [
-    {
-        name: "",
-        type: "grid",
-        schema: [
-            {
-                name: "from",
-                label: "From",
-                required: true,
-                selector: { number: {} },
-            },
-            {
-                name: "color",
-                label: "heading.entity_config.color",
-                required: true,
-                selector: { color_rgb: {} },
-            },
-        ],
-    },
-    {
-        name: "label",
-        label: "Label",
-        selector: { text: {} },
-    }
-];
 
 @customElement("modern-circular-gauge-editor")
 export class ModernCircularGaugeEditor extends LitElement {
@@ -161,6 +135,7 @@ export class ModernCircularGaugeEditor extends LitElement {
               {
                 name: "",
                 type: "grid",
+                column_min_width: "100px",
                 schema: [
                   {
                     name: "from",
@@ -198,6 +173,38 @@ export class ModernCircularGaugeEditor extends LitElement {
         selector: { boolean: {} },
       },
       {
+        name: "segments",
+        type: "mcg-list",
+        title: "Color segments",
+        iconPath: mdiSegment,
+        schema: [
+          {
+            name: "",
+            type: "grid",
+            column_min_width: "100px",
+            schema: [
+              {
+                name: "from",
+                label: "From",
+                required: true,
+                selector: { number: {} },
+              },
+              {
+                name: "color",
+                label: "heading.entity_config.color",
+                required: true,
+                selector: { color_rgb: {} },
+              },
+            ],
+          },
+          {
+              name: "label",
+              label: "Label",
+              selector: { text: {} },
+          }
+        ]
+      },
+      {
         name: "tap_action",
         selector: {
           ui_action: {
@@ -233,39 +240,6 @@ export class ModernCircularGaugeEditor extends LitElement {
         .computeLabel=${this._computeLabel}
         @value-changed=${this._valueChanged}
     ></ha-form>
-    <ha-expansion-panel outlined>
-        <div
-            slot="header"
-            role="heading"
-        >
-            <ha-svg-icon .path=${mdiSegment}></ha-svg-icon>
-            Color segments
-        </div>
-        <div class="content">
-            <ha-icon-button
-                .label=${this.hass.localize("ui.common.add")}
-                .path=${mdiPlus}
-                @click=${this._addSegment}
-            ></ha-icon-button>
-            ${DATA.segments?.map((row, index) => html`
-                <div class="segment-entry">
-                    <ha-form
-                        .hass=${this.hass}
-                        .data=${row}
-                        .schema=${SEGMENT}
-                        .index=${index}
-                        .computeLabel=${this._computeLabel}
-                        @value-changed=${this._segmentChanged}
-                    ></ha-form>
-                    <ha-icon-button
-                        .label=${this.hass.localize("ui.common.remove")}
-                        .path=${mdiClose}
-                        @click=${this._removeSegment}
-                    >
-                </div>
-                `)}
-        </div>
-    </ha-expansion-panel>
     `;
   }
 
@@ -276,44 +250,6 @@ export class ModernCircularGaugeEditor extends LitElement {
     if (label) return label;
     return schema.label;
   };
-
-  private _addSegment(ev: CustomEvent): void {
-    ev.stopPropagation();
-    const value = { from: 0, color: [0, 0, 0] } as SegmentsConfig;
-    if (!this._config?.segments) {
-      fireEvent(this, "config-changed", { config: { ...this._config, segments: [value] } });
-      return;
-    }
-
-    fireEvent(this, "config-changed", { config: { ...this._config, segments: [...this._config.segments, value] } });
-  }
-
-  private _removeSegment(ev: CustomEvent): void {
-    ev.stopPropagation();
-    if (!this.hass || !this._config) {
-      return;
-    }
-    const index = (ev.target as any).index;
-    const newSegment = this._config.segments?.concat();
-
-    newSegment?.splice(index, 1);
-
-    fireEvent(this, "config-changed", { config: { ...this._config, segments: newSegment } } as any);
-  }
-
-  private _segmentChanged(ev: CustomEvent): void {
-    ev.stopPropagation();
-    if (!this.hass || !this._config) {
-      return;
-    }
-    const value = ev.detail.value;
-    const index = (ev.target as any).index;
-    const newSegment = this._config.segments!.concat();
-
-    newSegment[index] = value;
-
-    fireEvent(this, "config-changed", { config: { ...this._config, segments: newSegment } } as any);
-  }
 
   private _valueChanged(ev: CustomEvent): void {
     let config = ev.detail.value as ModernCircularGaugeConfig;
@@ -348,21 +284,6 @@ export class ModernCircularGaugeEditor extends LitElement {
 
   static get styles() {
     return css`
-      .segment-entry {
-        display: flex;
-        flex-direction: row;
-        justify-content: center;
-        align-items: center;
-        margin-bottom: 24px;
-      }
-
-      .segment-entry ha-form {
-        flex: 1;
-      }
-      
-      ha-expansion-panel {
-        margin-top: 24px;
-      }
     `;
   }
 }
