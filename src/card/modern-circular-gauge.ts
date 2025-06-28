e93b742099bfd2439a1f4f111c4fdbb626a5e583
selvalt7 <52136675+selvalt7@users.noreply.github.com>
2025-06-28 13:29:00 +0200

    Fix icon overlap

--- src/card/modern-circular-gauge.ts
+++ src/card/modern-circular-gauge.ts
@@ -365,6 +365,8 @@ export class ModernCircularGauge extends LitElement {
     }
 
     const value = Number(templatedState ?? stateObj.state);
+    const iconCenter = !(this._config?.show_state ?? false) && (this._config?.show_icon ?? true);
+    const secondaryHasLabel = typeof this._config?.secondary != "string" && this._config?.secondary?.label;
 
     return html`
     <svg class="icon-container" viewBox="-50 -50 100 100" preserveAspectRatio="xMidYMid">
@@ -372,7 +374,8 @@ export class ModernCircularGauge extends LitElement {
         <div class="icon-wrapper" style="width: 100px; height: 100px;">
           <ha-state-icon
             class=${classMap({ "adaptive": !!this._config?.adaptive_icon_color, "big": !this._hasSecondary })}
-            style=${styleMap({ "color": gaugeForegroundStyle?.color && gaugeForegroundStyle.color != "adaptive" ? gaugeForegroundStyle.color : computeSegments(value, segments, this._config?.smooth_segments, this) })}
+            style=${styleMap({ "color": gaugeForegroundStyle?.color && gaugeForegroundStyle.color != "adaptive" ? gaugeForegroundStyle.color : computeSegments(value, segments, this._config?.smooth_segments, this),
+              "bottom": secondaryHasLabel && !iconCenter ? "9%" : undefined })}
             .hass=${this.hass}
             .stateObj=${stateObj}
             .icon=${iconOverride}
