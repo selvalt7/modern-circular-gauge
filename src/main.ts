dcec24df8396e85e0f9e96992bfe44d76a3c54a1
selvalt7 <52136675+selvalt7@users.noreply.github.com>
2025-07-16 22:20:16 +0200

    Display card version in web console

--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,5 @@
 import "./card/modern-circular-gauge";
 import "./badge/gauge-badge";
+import { version } from "../package.json";
+
+console.log(`%cModern Circular Gauge%cv${version}`, "background-color: #57BB30; color: #2C412D; padding: 4px 6px; font-weight: bold", "background-color: gray; padding: 4px 6px; font-weight: bold");
