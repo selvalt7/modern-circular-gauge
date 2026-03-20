b4818fee6d08c1a351fb9e9e1cedda68fada5f1b
selvalt7 <52136675+selvalt7@users.noreply.github.com>
2026-03-20 20:08:40 +0100

    Add needle config to GUI

--- src/components/const.ts
+++ src/components/const.ts
@@ -2,6 +2,6 @@ import { NeedleType } from "../card/type";
 
 
 export const NEEDLE_PATHS: Partial<Record<NeedleType, string>> = {
-  "arrow": "M 4.5 0 L -4.5 -3 L -3 0 L -4.5 3 Z",
+  "arrow": "M -4.5 0 L 4.5 3 L 3 0 L 4.5 -3 Z",
   "line": "M -3.5 0 L 3.5 0 Z",
 };
